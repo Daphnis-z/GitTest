7c17724dde9c29ff17d47268a723b45306ff1781
Daphnis <daphnisz@163.com>
2015-10-22 12:25:43 +0800

    原始

---
+++ wpfPractice/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace wpfPractice
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if(textBoxInput.Text!="")
+            {
+                FileStream fStream = new FileStream("BinaryTest.txt", FileMode.Create);
+                BinaryWriter bWrite = new BinaryWriter(fStream,Encoding.ASCII);
+                bWrite.Write(textBoxInput.Text);
+                bWrite.Flush();
+                bWrite.Close();
+                fStream.Close();
+                MessageBox.Show("保存成功");
+            }
+        }
+
+        private void btnShow_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists("BinaryTest.txt"))
+            {
+                FileStream fStream = new FileStream("BinaryTest.txt", FileMode.Open);
+                BinaryReader bReader = new BinaryReader(fStream);
+                labShow.Content = bReader.ReadString();
+                bReader.Close();
+                fStream.Close();
+            }
+
+        }
+    }
+}
